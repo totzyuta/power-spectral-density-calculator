6fed709efb9be6b2cf3ba6bc90acdd47bd20fe3c
totz <yuta.totz@gmail.com>
2015-05-12 06:34:30 +0900

    Modify dft.c

--- dft.c
+++ dft.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
-#define DATASIZE 100000
+#define DATASIZE 1024
 #define PI 3.1415926535 // 円周率
 #define DELTA 0.00002   // サンプリング周期
 
 // フーリエ変換
-int dft(int *data /*[入力] 実数数値列としての信号: 配列)*/ ){
+void dft(double data[DATASIZE] /*[入力] 実数数値列としての信号: 配列)*/ ){
   // 配列の要素数を格納
-  const int N = sizeof data /sizeof data[0];
+  // const int N = sizeof data /sizeof data[0];
+  int N = DATASIZE;
   double result[N];
   for(int k=0; k <= N/2-1; ++k) {
-    double Re = 0;
-    double Im = 0;
+    double Re=0;
+    double Im=0;
     for(int n=0; n<N; ++n) {
-        double tht = 2*PI*k*n/N;
-        Re += data[n] * cos(tht);
-        Im += data[n] * sin(tht);
+      double tht = 2*PI*k*n/N;
+      Re += data[n] * cos(tht);
+      Im += data[n] * sin(tht);
+      // printf("Re(%d): %f\n", n,Re);
     }// i
     Re = pow(Re, 2.0);
     Im = pow(Im, 2.0);
     double delta;
     delta = pow(DELTA, 2.0);
-    result[k] = delta*(Re+Im);
+    data[k] = delta*(Re+Im);
+    printf("%d, %e\n", k, result[k]);
   }// j
-  return *result;
 }
 
 
@@ -44,8 +46,20 @@ int main(int argc, char *argv[])
   /* ファイルの終端まで文字を読み取り表示する */
   // while ( fgets(readline, DATASIZE, fp) != NULL ) {
   while( fscanf(fp,"%lf",&data[i]) != EOF ){
-    printf("%f\n", data[i]);
+    // printf("%f\n", data[i]);
     i++;
+  }
+
+  FILE *wfp;
+  char *wfilename = "result.csv";  
+  wfp = fopen(wfilename,"w");     //ファイルのオープン
+
+  // フーリエ変換を行う
+  dft(data);
+
+  for (int j=0; j<DATASIZE/2; j++) {
+    printf("%d: %e\n", j, data[j]);
+    fprintf(wfp,"%.32f\n",data[j]) ;
   }
 
   /* ファイルのクローズ */
