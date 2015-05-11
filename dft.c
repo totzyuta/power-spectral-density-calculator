1a42b015f9b95b6c3f9ac132247f8029ef27fd40
totz <yuta.totz@gmail.com>
2015-05-11 20:33:56 +0900

    Modify dft.c

--- dft.c
+++ dft.c
@@ -1,97 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 
-/* 関数のデータを求めるプログラム例 */
+#define DATASIZE 100000
+#define PI 3.1415926535 // 円周率
+#define DELTA 0.00002   // サンプリング周期
 
-#define max 100 // Max number to loop
-
-int main()
-{
-  int i,n,m;
-  double x,y[max];
-  
-  n=0;
-  
-  FILE *function;
-  function=fopen("function.data","w");
-
-  //のこぎり波の一周期分の数値をつくる。
-  for(x=-1.0;x<=1.0;x+=0.1) {
-    y[n]=x;
-    n++;
-  }
-
-  //のこぎり波の生成
-  for(i=0;i<max;i++) {
-    for(m=0;m<n;m++) {
-      fprintf(function,"%f\n",y[m]);
-    }
-  }
-
-  fclose(function);
-
-  return 0;
-}
-
-/* フーリエ変換してスペクトルの強さを求めるプログラム例 */
-
-#define max 10000                       //Nの限界値
-#define pi 3.1415926535         //円周率
-
-int dft()
-{
-  int k,n,N;
-  double f[max+1],ReF/*実数部*/,ImF/*虚数部*/;
-
-  FILE *function;
-  FILE *fourier;
-
-  //フーリエ変換したいデータをあらかじめファイル function.data に保存しておく
-  function=fopen("function.data","r");
-  fourier=fopen("fourier.data","w");
-
-  //データの読み込み（ただし実数の成分のみ）
-  for(N=0;N<max;N++) {
-    if(fscanf(function,"%lf",&f[N]) == EOF) {
-      N--;
-      break;
-    }
-  }
-
-  //実数部分と虚数部分に分けてフーリエ変換
-  for(n=0;n<N;n++) {
-    ReF=ImF=0.0;
-    for(k=0;k<N;k++) {
-      ReF +=  f[k] * cos(2*pi*k*n/N); // 実数部分cosの計算
-      ImF += -f[k] * sin(2*pi*k*n/N); // 虚数部分sinの計算
-    }
-    fprintf(fourier,"%d %f %f %f\n",n,ReF,ImF,ReF*ReF+ImF*ImF); //最後はスペクトルの強さ
-  }
-
-  fclose(function);
-  fclose(fourier);
- 
-  double result[3] = [ReF, ImF, ReF*ReF+ImF*ImF];
-  return 0;
-}
-
-#define PI 3.1415926535         //円周率
-
-// TODO: メインで作ってるやつ
-double dft(int *a /*[入力] 実数数値列としての信号: 配列)*/ ){
+// フーリエ変換
+int dft(int *data /*[入力] 実数数値列としての信号: 配列)*/ ){
   // 配列の要素数を格納
-  const int N = sizeof a / sizeof a[0];
-  for( int k=0; k<N; ++k ) {
-    double Re_sum = 0;
-    double Im_sum = 0;
-    for( int n=0; n<N; ++n ) {
+  const int N = sizeof data /sizeof data[0];
+  double result[N];
+  for(int k=0; k <= N/2-1; ++k) {
+    double Re = 0;
+    double Im = 0;
+    for(int n=0; n<N; ++n) {
         double tht = 2*PI*k*n/N;
-        Re_sum += a[n] *cos( tht );
-        Im_sum += a[n] *sin( tht );
+        Re += data[n] * cos(tht);
+        Im += data[n] * sin(tht);
     }// i
-    // todo: こっから05-03
-    Re->push_back( Re_sum );
-    Im->push_back( Im_sum );
+    Re = pow(Re, 2.0);
+    Im = pow(Im, 2.0);
+    double delta;
+    delta = pow(DELTA, 2.0);
+    result[k] = delta*(Re+Im);
   }// j
-  return;
+  return *result;
+}
+
+
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  char *filename = argv[1];
+  char readline[DATASIZE] = {'\0'};
+
+  /* ファイルのオープン */
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "%sのオープンに失敗しました.\n", filename);
+  }
+
+  int i=0;
+  double data[DATASIZE];
+  /* ファイルの終端まで文字を読み取り表示する */
+  // while ( fgets(readline, DATASIZE, fp) != NULL ) {
+  while( fscanf(fp,"%lf",&data[i]) != EOF ){
+    printf("%f\n", data[i]);
+    i++;
+  }
+
+  /* ファイルのクローズ */
+  fclose(fp);
+
+  return 0;
 }
